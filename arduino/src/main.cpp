9f12b7a4c794d686f3a60196940532a2878c454c
inventor <inventor.deus@proton.me>
2026-05-29 11:31:57 +0200

    First challenge prototype

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,23 +1,37 @@
-// www.elegoo.com
-// 2016.12.08
-
-// #include "Arduino.h"
+#include "Arduino.h"
 #include <HCSR04.h>
 
-// Initialize sensor that uses digital pins 13 and 12.
-const byte triggerPin = 13;
-const byte echoPin = 12;
+const byte RED = 7;
+
+// Initialize the ultrasonic sensor
+const byte triggerPin = 12;
+const byte echoPin = 11;
 UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
 
+
+bool bombActive = true;
+
 void setup() {
-  Serial.begin(9600); // We initialize serial connection so that we could print
-                      // values from sensor.
+  Serial.begin(9600); // We initialize serial connection so that we could print values from sensor.
+  pinMode(RED, INPUT_PULLUP);
 }
 
 void loop() {
-  // Every 500 miliseconds, do a measurement using the sensor and print the
-  // distance in centimeters.
+
+  int redWire = !digitalRead(RED);
   float distance = distanceSensor.measureDistanceCm();
-  Serial.println(distance);
-  delay(500);
+
+  if (redWire == 0 && bombActive) {
+    bombActive = false;
+    if (distance > 20 && distance < 30) {
+      Serial.println("Bomb has been defused!");
+    } else {
+      Serial.println("BOOOOOOOOOOOOOOOOOOOM!");
+    }
+  }
+
+  // Reset the bomb
+  if (redWire == 1) {
+    bombActive = true;
+  }
 }
