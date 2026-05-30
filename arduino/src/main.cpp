2665b9bca402a5bd9ce9288428a306fa84f6277b
aburch-quack <aburch00@duck.com>
2026-05-30 12:19:51 +0200

    Final timer script

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,23 +1,81 @@
-// www.elegoo.com
-// 2016.12.08
+#include <Arduino.h>
 
-// #include "Arduino.h"
-#include <HCSR04.h>
+float initialTime = 150.0;
+float time = initialTime;
 
-// Initialize sensor that uses digital pins 13 and 12.
-const byte triggerPin = 13;
-const byte echoPin = 12;
-UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
+bool combinations[12][8] = {
+  {1, 1, 1, 1, 1, 1, 0, 0},
+  {0, 1, 1, 0, 0, 0, 0, 0},
+  {1, 1, 0, 1, 1, 0, 1, 0},
+  {1, 1, 1, 1, 0, 0, 1, 0},
+  {0, 1, 1, 0, 0, 1, 1, 0},
+  {1, 0, 1, 1, 0, 1, 1, 0},
+  {1, 0, 1, 1, 1, 1, 1, 0},
+  {1, 1, 1, 0, 0, 0, 0, 0},
+  {1, 1, 1, 1, 1, 1, 1, 0},
+  {1, 1, 1, 1, 0, 1, 1, 0},
+  {1, 1, 1, 1, 1, 1, 1, 1},
+  {0, 0, 0, 0, 0, 0, 0, 0}
+};
 
 void setup() {
-  Serial.begin(9600); // We initialize serial connection so that we could print
-                      // values from sensor.
+  // put your setup code here, to run once:
+  for (int i = 2; i < 14; i++)
+  {
+      pinMode(i, OUTPUT);
+  }
+  pinMode(0, INPUT_PULLUP);
+}
+
+void setCombination(bool combination[8])
+{
+  for (int i = 0; i < 8; i++)
+  {
+    digitalWrite(i + 6, combination[i] ? HIGH : LOW);
+  }
+}
+
+void lightDigit(int digit, int number)
+{
+  setCombination(combinations[number]);
+  digitalWrite(digit+1, LOW);
+  delay(1);
+  time -= 0.001;
+  digitalWrite(digit+1, HIGH);
+}
+
+void lightAll(int number)
+{
+  setCombination(combinations[number]);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  delay(1);
+  time -= 0.001;
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
 }
 
 void loop() {
-  // Every 500 miliseconds, do a measurement using the sensor and print the
-  // distance in centimeters.
-  float distance = distanceSensor.measureDistanceCm();
-  Serial.println(distance);
-  delay(500);
+  if (digitalRead(0) == LOW)
+  {
+    time = initialTime;
+  }
+  int minutes = floor(max(0, ceil(time)) / 60.0);
+  int seconds = (int)max(0, ceil(time)) % 60;
+  lightDigit(1, floor(minutes / 10.0));
+  lightDigit(2, minutes % 10);
+  lightDigit(3, floor(seconds / 10.0));
+  lightDigit(4, seconds % 10);
+  if (time - floor(time) > 0.5) digitalWrite(13, HIGH);
+  else digitalWrite(13, LOW);
+  delay(1);
+  if (time == 0)
+  {
+    digitalWrite(1, LOW);
+  }
+  else digitalWrite(1, HIGH);
 }
